1c6f7d6b2aa3e77d1aa9b35384cd977e7f1c7bb3
Kev Grav <kgravell.prsvr@gmail.com>
2024-07-15 11:42:07 -0400

    first done

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(logger("dev"))
 app.use(express.json())
 
 
-const products = [
+const store = [
     {
         name: 'apple',
         price: 1.5
@@ -16,11 +16,11 @@ const products = [
 ]
 
 app.get('/', (request, response)=>{
-    response.json({message:"all products found", payload:products})
+    response.json({message:"all products found", payload:store})
 })
 
 app.get('/get-all-products', (request, response)=>{
-    response.json({message:"products found", payload:products.name})
+    response.json({message:"products found", payload:store.name})
 
 })
 
@@ -29,11 +29,37 @@ app.get('/get-product/:productName', (req, res)=> {
 })
 
 app.post('/create-product', (request, response)=>{
-    products.push({
+    store.push({
         name: request.body.name,
         price: request.body.price
     })
     response.json({message: "Product added"})
 })
 
+//5
+app.post('/create-product', (request, response)=>{
+    const product = store.find(product => product.name === req.params.name)
+    if(product){
+        res.json({message: `${product.name} already exists.`})
+    }else{
+        store.push({
+        name: request.body.name,
+        price: request.body.price
+        })
+    res.json({message: "Product added"})
+    }
+})
 
+
+//6
+app.delete('/delete-product', (req, res)=>{
+    //find product by name and delete
+    const {name} = req.body
+    const productIndex = store.findIndex(product => product.name === name)
+    if(productIndex !== -1){
+        store.splice(productIndex, 1)
+        return res.json({message: "Product deleted", payload: store})
+    }else{
+        return res.json({message: "Product not found"})
+    }
+})
